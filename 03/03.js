e92115fbf66321193b82fec80b0759ea7e4934d1
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-12-07 15:45:33 +0530

    changes to 03

--- 03/03.js
+++ 03/03.js
@@ -6,7 +6,7 @@ var minus = one - two;
 var multiply = one * two;
 var divide = one/two;
 
-console.log("First No: = " + one + " Second No: = " + two);
+console.log("First No:  " + one + " Second No: " + two);
 console.log(one + " + " + two + " = " + add);
 console.log(one + " - " + two + " = " + minus);
 console.log(one + " * " + two + " = " + multiply);
