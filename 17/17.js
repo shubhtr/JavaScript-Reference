98b7be28dde162292663179053ca30375840f4b7
Shubhrendu Tripathi <11039534+shubhrendutripathi@users.noreply.github.com>
2020-12-29 11:23:33 +0530

    added folder 17

---
+++ 17/17.js
@@ -0,0 +1,9 @@
+//create cookie
+
+function createCookie(cookieName, cookieValue, daysToExpire) {
+	var date = new Date();
+	date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+	document.cookie = cookieName + "=" + cookieValue + "; expires=" + date.toGMTString();
+}
+
+
